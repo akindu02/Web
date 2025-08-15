ff1710772a7e7048fb946845745e21c48851da91
Akindu Samarasinghe <samaras-im22022@stu.kln.lk>
2025-08-15 15:10:51 +0530

    vs code changes

---
+++ script.js
@@ -0,0 +1,4 @@
+function changeContent() {
+  document.getElementById('main-title').textContent = 'Hello from JavaScript!';
+  document.getElementById('main-paragraph').textContent = 'The text has been changed.';
+}
