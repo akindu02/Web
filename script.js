55b2b3b317d0209f58f5dc47c7e1ab0f68d7d4eb
Akindu Samarasinghe <samaras-im22022@stu.kln.lk>
2025-08-17 11:31:50 +0530

    delete files

--- script.js
+++
@@ -1,4 +0,0 @@
-function changeContent() {
-  document.getElementById('main-title').textContent = 'Hello from JavaScript!';
-  document.getElementById('main-paragraph').textContent = 'The text has been changed.';
-}
